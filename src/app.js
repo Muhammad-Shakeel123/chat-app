e5d0c645c4f0cf6b39daf2251eb3ea4d99aab67f
Muhammad-Shakeel123 <muhammadshakeelafd@gmail.com>
2025-03-01 20:42:20 +0500

    Fix Vercel ENOENT issue

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -22,8 +17,6 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 
-// ✅ Serve Static Files
-app.use(express.static(path.join(__dirname, 'public')));
 
 // ✅ Import Routes
 import authRoutes from './routes/auth.routes.js';
