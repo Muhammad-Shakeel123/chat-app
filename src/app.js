53f1ceaa557d2a6b730c21a5f1f945d14a49bbd4
Muhammad-Shakeel123 <muhammadshakeelafd@gmail.com>
2025-03-01 20:34:04 +0500

    Fix Vercel ENOENT issue

--- src/app.js
+++ src/app.js
@@ -38,8 +38,9 @@ app.use('/api/v1/message', messageRoutes);
 
 
 // ✅ Serve Frontend (public/index.html)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+  res.send('Backend is running...');
 });
+
 
 export { app };
