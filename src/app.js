f4cda6f5a7a602495d696fd4bb3ff8b5bd5684bc
Muhammad-Shakeel123 <muhammadshakeelafd@gmail.com>
2025-03-02 16:44:22 +0500

    Fix Swagger UI static file serving

--- src/app.js
+++ src/app.js
@@ -22,7 +22,12 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+app.get('/favicon.ico', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'favicon.ico')),
+);
+app.get('/favicon.png', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'favicon.png')),
+);
 
 // ✅ Import Routes
 import authRoutes from './routes/auth.routes.js';
